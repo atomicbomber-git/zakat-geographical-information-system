916dcbebc4b4386fcd078ccf070e30e03db36633
James Patrick Keegan <atomicbomber.email@gmail.com>
2019-12-08 23:29:12 +0700

    Updates.

---
+++ database/migrations/2019_12_08_232346_change_bigint_to_int_in_informations.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Support\Facades\DB;
+
+class ChangeBigintToIntInInformations extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('information', function (Blueprint $table) {
+            DB::select("
+                ALTER TABLE information MODIFY COLUMN id INT(11)
+            ");
+
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('information', function (Blueprint $table) {
+            DB::select("
+                ALTER TABLE information MODIFY COLUMN id BIGINT(11)
+            ");
+        });
+    }
+}
