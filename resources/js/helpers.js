bde2915182fe685c485c218e7ba4d48a2f5efe85
James Patrick Keegan <atomicbomber.email@gmail.com>
2019-12-10 08:52:41 +0700

    Updates.

--- resources/js/helpers.js
+++ resources/js/helpers.js
@@ -4,20 +4,20 @@ import numeral from 'numeral'
 
 // Convert degree to radian
 export function deg2rad(deg) {
-    return deg * (Math.PI/180)
+    return deg * (Math.PI / 180)
 }
 
 // Returns distance in kilometers
 export function getDistance(lat1, lon1, lat2, lon2) {
-    let R = 6371 // Radius of the earth in km
-    let dLat = deg2rad(lat2-lat1)  // deg2rad below
-    let dLon = deg2rad(lon2-lon1)
+    let R = 6371 // R = Radius bumi dalam satuan KM
+    let dLat = deg2rad(lat2 - lat1) // dLat diisi dengan selisih dari lat2 dan lat1 yang telah dikonversi ke satuan radian dari satuan derajat
+    let dLon = deg2rad(lon2 - lon1) // dLon diisi dengan selisih dari lon2 dan lon1 yang telah dikonversi ke satuan radian dari satuan derajat
     let a =
-        Math.sin(dLat/2) * Math.sin(dLat/2) +
-        Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) *
-        Math.sin(dLon/2) * Math.sin(dLon/2);
-    let c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
-    let d = R * c;
+        Math.sin(dLat / 2) * Math.sin(dLat / 2) + // Sin (dLat / 2) * Sin(dLat / 2) +
+        Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * // Cos (lat1) * Cos(lat2) *
+        Math.sin(dLon / 2) * Math.sin(dLon / 2); // Sin(dLon / 2) * Sin(dLon / 2)
+    let c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a)); // c = 2 * ArcTan(Akar(A), Akar(1 - A))
+    let d = R * c; // d (jarak) = R (radius bumi) * c
     return d;
 }
 
